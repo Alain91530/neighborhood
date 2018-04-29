11931f9b59f1b19a7fee1708832f588b82de1d95
Unknown <alain.cadenat@depannum.com>
2018-04-29 16:14:53 +0200

    refactor

---
+++ src/AppHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+//import '../styles/App.css';
+
+
+class AppHeader extends Component {
+
+  render() {
+
+    return(
+      <header>
+        <h1>Herault's places of interest</h1>
+      </header>
+    );
+  }
+}
+export default AppHeader;
